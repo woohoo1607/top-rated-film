9d441d097500f75162c0af23ca7db112c7019502
woohoo1607 <45455797+woohoo1607@users.noreply.github.com>
2019-09-10 19:57:19 +0300

    Updates

---
+++ precache-manifest.212c1bf05635b880e512ef49848f8b4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804c0910ffb4f79e9ba375b023184127",
+    "url": "/top-rated-film/index.html"
+  },
+  {
+    "revision": "1360ebf4418b9c0d646f",
+    "url": "/top-rated-film/static/css/main.9419ef2e.chunk.css"
+  },
+  {
+    "revision": "87f9c3ea9b4d90c3a1a2",
+    "url": "/top-rated-film/static/js/2.e68ca3b0.chunk.js"
+  },
+  {
+    "revision": "1360ebf4418b9c0d646f",
+    "url": "/top-rated-film/static/js/main.a462f433.chunk.js"
+  },
+  {
+    "revision": "2589bcd7ce449d648994",
+    "url": "/top-rated-film/static/js/runtime~main.0c33981d.js"
+  },
+  {
+    "revision": "8a001a35c72d349af8627fdfc77716f7",
+    "url": "/top-rated-film/static/media/darkBackground.8a001a35.jpg"
+  },
+  {
+    "revision": "59b65ba86efff7cbe4ed16ce4c8fd319",
+    "url": "/top-rated-film/static/media/loading.59b65ba8.gif"
+  }
+]);
